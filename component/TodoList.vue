529947aa8df318a83a0bd7bc317abe8f49703e44
Supriya <supriya@volopay.co>
2025-04-08 22:44:01 +0530

    setup vue

---
+++ component/TodoList.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+    <div class="p-6 text-center text-blue-600">
+      Hello Tailwind + Nuxt 💙
+    </div>
+  </template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+  .p-6.text-center.text-blue-600
+    | Hello Tailwind + Nuxt 💙
+
+</template>
